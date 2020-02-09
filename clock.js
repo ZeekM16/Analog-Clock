f451802ed23c8b90007b664e6fb51f7afac4d3cd
Isaac Molina <isaacmolina.m16@gmail.com>
2020-02-08 20:19:25 -0600

    updated drawHand/Time

--- clock.js
+++ clock.js
@@ -9,6 +9,7 @@ drawClock();
 function drawClock(){
    drawFace(ctx, radius);
    drawNumbers(ctx, radius);
+   drawTime(ctx, radius);
 }
 
 function drawFace(){
@@ -58,5 +59,12 @@ function drawTime(ctx, radius){
     hour=(hour*Math.PI/6)+
     (minute*Math.PI/(6*60))+
     (second*Math.PI/(360*60));
-    
+    drawHand(ctx, hour, radius*0.5, radius*0.07);
+
+    minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+    drawHand(ctx, hour, radius*0.8, radius*0.07);
+
+    second=(second*Math.PI/30);
+    drawHand(ctx, hour, radius*0.9, radius*0.02);
+
 }
